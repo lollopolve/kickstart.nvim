296be88f4cdff2d8784d0f69019f46d8037dba45
lollopolve <lorenzo.polverelli13@gmail.com>
2024-02-26 20:53:28 +0100

    upgraded packages and formatting

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -32,8 +32,8 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = highlight_group,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	group = highlight_group,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
